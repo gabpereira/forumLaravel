9f7dbad6750a190f3daa3ebdbbdce44d6698a4d2
Gabriel Pereira <54536712+gabpereira@users.noreply.github.com>
2020-12-08 11:18:46 +0100

    Add API

--- app/Http/Resources/ReferenceResourceCollection.php
+++ app/Http/Resources/ReferenceResourceCollection.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ReferenceCollection extends ResourceCollection
+class ReferenceResourceCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
@@ -14,9 +14,6 @@ class ReferenceCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return [
-            'description' => $this->description,
-            'url' => $this->url
-        ];
+        return ReferenceCollection::collection($this->collection);
     }
 }
