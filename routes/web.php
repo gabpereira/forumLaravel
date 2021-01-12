0f3236961994a0a2d4f110027df7beba3f6f41aa
Gabriel Pereira <54536712+gabpereira@users.noreply.github.com>
2021-01-12 10:03:26 +0100

    breez install

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\ReferenceController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\StateController;
 use App\Http\Controllers\TopicController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,3 +28,8 @@ Route::Resource('states',StateController::class);
 Route::Resource('themes',ThemeController::class);
 Route::Resource('topics',TopicController::class);
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
